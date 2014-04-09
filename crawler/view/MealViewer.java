ed0595c4e0798a7d67dfaadeb9d39b283c23be20
Vinícius Angiolucci Reis <angiolucci@gmail.com>
2014-04-08 22:03:18 -0300

    Melhorias na visualização

--- crawler/view/MealViewer.java
+++ crawler/view/MealViewer.java
@@ -5,7 +5,6 @@ import crawler.model.Meal;
 public class MealViewer {
 	private Meal[] meals = null;
 
-
 	public MealViewer(Meal[] meals) {
 		if (meals != null && meals.length == 10)
 			this.meals = meals;
@@ -15,16 +14,28 @@ public class MealViewer {
 		if (this.meals != null)
 			for (int i = 0; i < this.meals.length; i++) {
 				if (i % 2 == 0)
-					System.out.println("========== ALMOÇO ==========");
+					System.out
+							.println("=================== ALMOÇO ===================");
 				else
-					System.out.println("========== JANTAR ==========");
+					System.out
+							.println("=================== JANTAR ===================");
 
 				System.out.println("Data: " + meals[i].getDayOfWeek() + ", "
 						+ meals[i].getStrData());
-				System.out.println("Principal: " + meals[i].getPrinc() + " / "
-						+ meals[i].getVegPrinc());
-				System.out.println("Guarnição: " + meals[i].getGuarn() + " / "
-						+ meals[i].getVegGuarn());
+
+				if (meals[i].getPrinc()
+						.equalsIgnoreCase(meals[i].getVegPrinc()))
+					System.out.println("Principal: " + meals[i].getPrinc());
+				else
+					System.out.println("Principal: " + meals[i].getPrinc()
+							+ " / " + meals[i].getVegPrinc());
+				if (meals[i].getGuarn()
+						.equalsIgnoreCase(meals[i].getVegGuarn()))
+					System.out.println("Guarnição: " + meals[i].getGuarn());
+				else
+					System.out.println("Guarnição: " + meals[i].getGuarn()
+							+ " / " + meals[i].getVegGuarn());
+				
 				System.out.println("Salada: " + meals[i].getSalad());
 				System.out.println("Sobremesa: " + meals[i].getSobrm());
 				System.out.println("\n");
